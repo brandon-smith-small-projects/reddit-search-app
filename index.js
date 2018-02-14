59f629ea254168078f1b13c3380feac43a57c5f2
Brandon Smith <brandon@reddirtwebdesign.com>
2018-02-13 17:02:43 -0700

    Output Search Results

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import reddit from './redditapi';
+
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
@@ -17,6 +19,45 @@ searchForm.addEventListener('submit', e => {
     //Show Message
     showMessage('Please add a search term', 'alert-danger');
   }
+
+  //Clear Input
+  searchInput.value = '';
+
+  //Search Reddit
+  reddit.search(searchTerm, searchLimit, sortBy).then(res => {
+    if (res.length === 0) {
+      showMessage('No Results Found', 'alert-info');
+    } else {
+      let output = `
+      <div class='card-columns'>`;
+      //Loop through Posts
+      res.forEach(post => {
+        //Check For Image
+        const image = post.preview
+          ? post.preview.images[0].source.url
+          : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+        output += `
+        <div class="card">
+          <img class="card-img-top" src="${image}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${post.title}</h5>
+            <p class="card-text">${truncateText(post.selftext, 100)}</p>
+            <a href="${
+              post.url
+            }" target="_blank" class="btn btn-primary">Read More</a>
+            <hr>
+            <span class="badge badge-secondary">Subreddit:${
+              post.subreddit
+            }</span>
+            <span class="badge badge-dark">Subreddit:${post.score}</span>
+          </div>
+        </div>
+        `;
+      });
+      output += `<div>`;
+      document.getElementById('results').innerHTML = output;
+    }
+  });
 
   e.preventDefault();
 });
@@ -39,4 +80,15 @@ function showMessage(message, className) {
   setTimeout(() => {
     document.querySelector('.alert').remove();
   }, 3000);
+}
+
+//Truncate Text
+function truncateText(text, limit) {
+  const shortened = text.indexOf(' ', limit);
+
+  if (shortened === -1) {
+    return text;
+  }
+
+  return text.substring(0, shortened);
 }
